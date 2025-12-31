f7fb350e7a1b6a93db5de6ad771f4c92be7faff8
High Bridge <hainu738@gmail.com>
2025-12-31 23:33:08 +0900

    add SPDX

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2025 highbrige-yayoi <hainu738@gmail.com>
+# SPDX-License-Identifier: BSD-3-Clause
+
 from setuptools import find_packages, setup
 import os
 from glob import glob
